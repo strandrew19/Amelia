3856b1579f15efdd3186f8a1099d506c87c47e7d
David Schischke <hi@davidschischke.com>
2022-01-05 20:29:08 +0100

    Fixed sampling correction methods and added relevance function. Working now, but not doing what they should be doing yet.

--- 02 Sampling Correction methods.R
+++ 02 Sampling Correction methods.R
@@ -1,8 +1,6 @@
 # Read data first as RStudio gets stuck on read_feather command otherwise
 
-library(arrow)
-setwd("~/Uni/02 Data Science/03 Wise 21-22/Research Case Studies/Code/Amelia")
-amelia_full_income <- read_feather("data/AMELIA.feather")$Person_Income
+amelia_full_income <- arrow::read_feather("~/Uni/02 Data Science/03 Wise 21-22/Research Case Studies/Code/Amelia/data/AMELIA.feather")$Person_Income
 
 library(dplyr)
 library(UBL)
@@ -13,56 +11,54 @@ source("functions/relevance_function.R")
 
 
 # Set bucket size for comparison between values 
-BUCKET_SIZE <- 5000
-formals(get_bucket_dist)$bucket_size <- BUCKET_SIZE # Set as global preset
+BUCKET_SIZE <- 4000
 formals(compute_income_diff)$bucket_size <- BUCKET_SIZE # Set as global preset
 
+# Get maximum income 
+MAX <- max(amelia_full_income)
+formals(compute_income_diff)$max_val <- MAX
 
 #### Computing true buckets #####
-AMELIA_BUCKET_INCOME <- get_bucket_dist(amelia_full_income)
+AMELIA_BUCKET_INCOME <- get_bucket_dist(amelia_full_income, AMELIA = T, max_val = MAX, bucket_size = BUCKET_SIZE)
 formals(compute_income_diff)$amelia_income_dist <- AMELIA_BUCKET_INCOME # Set as global preset
 
 #### Computing sample difference ####
-
-FILENAMES <- str_pad(1:10, width = 2, pad = "0")
-FILENAMES <- paste0(FILENAMES, "_sample.rds", sep = "")
-
-SAMPLING_METHODS <- c("SRS", "Stratified", "Stratified Cluster")
-
-
+# FILENAMES <- str_pad(1:10, width = 2, pad = "0")
+# FILENAMES <- paste0(FILENAMES, "_sample.rds", sep = "")
+# 
+# SAMPLING_METHODS <- c("SRS", "Stratified", "Stratified Cluster")
 
 
 ex_sample <- readRDS("data/samples/03_sample.rds")
-sample_difference <- compute_income_diff(ex_sample$SRS$Base$Person_Income, plot = T)
+ex_sample$SRS$Base$Sex <- as.factor(ex_sample$SRS$Base$Sex)
+
+sample_difference <- compute_income_diff(ex_sample$SRS$Base$Person_Income, plot = T, plot_relevance = T)
 
 #### Importance sampling ####  
 
-#' Important: These functions assume to receive a data.frame object and they will not work with 
-#' other formats, e.g. a tibble (usually returned by dplyr functions).
+#' Note:
+#' These functions assume to receive a data.frame object and they will not work with 
+#' other formats, e.g. a tibble (usually returned by dplyr functions, e.g. when sampling).
 
 REGRESSION_FORMULA <- Person_Income ~ AGE * Sex + Work_Status
-
 resample_data <- ex_sample$SRS$Base
-relevance_function <- sample_difference$scaled_relevance_table
-relevance_threshold <- sample_difference$relevance_threshold$mean
 
 ### Relevance function must be supplied as a matrix of form y, phi(y), phi'(y)
 
-buckets <- seq(0, 1034300, BUCKET_SIZE)
-relevance_per_bucket <- as.numeric(relevance_function)
-relevance_deriv_per_bucket <- rep(0,length(buckets))
-
-relevance_matrix <- matrix(cbind(buckets, relevance_per_bucket, relevance_deriv_per_bucket), ncol = 3)
-
-WERCSRegress(
+imp_samp <- WERCSRegress(
   form = REGRESSION_FORMULA, 
   dat = resample_data,
-  rel = relevance_matrix)
+  rel = sample_difference$relevance_matrix_ubl)
 
-# SmoteRegress(form = REGRESSION_FORMULA, 
-#              dat = ex_sample$SRS$Base, 
-#              dist = "HEOM", 
-#              rel = relevance_matrix)
+compute_income_diff(imp_samp$Person_Income, plot = T)
+
+smote <- SmoteRegress(form = REGRESSION_FORMULA,
+             dat = resample_data,
+             dist = "HEOM", 
+             rel = sample_difference$relevance_matrix_ubl,
+             k = 5)
+
+compute_income_diff(smote$Person_Income, plot = T)
 
 #### Simulation #### 
 
--- functions/relevance_function.R
+++ functions/relevance_function.R
@@ -1,15 +1,16 @@
 library(ggplot2)
 rescale <- scales::rescale
 
-get_bucket_dist <- function(x, bucket_size, max_val = 1034300){
+get_bucket_dist <- function(x, buckets, AMELIA = F, max_val = NULL, bucket_size = NULL){
   #' This function computes the buckets for a given income sample, which is needed to compare it 
   #' to the known information about the income distribution of the full dataset.
   #' 
   #' INPUTs
   #'   - x           = income of sample
-  #'   - bucket_size = size of outgoing buckets, should be set once and always kept constant
-  #'   - max_val     = maximum income in AMELIA dataset (to ensure that we always have same amount 
-  #'                   of categories)
+  #'   - buckets     = vector of buckets based on AMELIA dataset
+  #'   - AMELIA      = T/F variable, indicates whether buckets on original AMELIA data are computed
+  #'   - max_val     = maximum income value, only relevant if AMELIA == T
+  #'   - bucket_size = size of income buckets, only relevant if AMELIA == T
   #'  
   #'  OUTPUT
   #'    - a list with
@@ -17,10 +18,12 @@ get_bucket_dist <- function(x, bucket_size, max_val = 1034300){
   #'      - the proportion of people per income class (prop.table)
   
   
-  min <- 0
-  max <- max_val + bucket_size
-  
-  buckets <- seq(min, max, bucket_size)
+  if (AMELIA){
+    min <- 0
+    max <- max_val + bucket_size
+    
+    buckets <- seq(min, max, bucket_size)
+  }
   
   dist_val <- table(cut(x, breaks = buckets, include.lowest = T))
   dist_prop <- prop.table(dist_val)
@@ -147,7 +150,7 @@ plot_income_diff <- function(sample_data, amelia, bucket_size, label_distance, p
 }
 
 
-compute_income_diff <- function(sample_income, amelia_income_dist, bucket_size, 
+compute_income_diff <- function(sample_income, amelia_income_dist, bucket_size, max_val, 
                                 padding = 0, damp = "pop",
                                 plot = F, label_distance = 10, show_mean = F, show_median = F, sample_type = NULL, plot_relevance = F){
   #' This function is a wrapper for all the other function in this file. Note that we use the 'dist_prop'
@@ -163,7 +166,9 @@ compute_income_diff <- function(sample_income, amelia_income_dist, bucket_size,
   #' INPUT
   #'   - sample_income      = Vector containing the income values of all the people in the sample
   #'   - amelia_income_dist = Distribution of the AMELIA income in buckets, i.e. the output 
-  #'                          of the get_bucket_dist function for the full AMELIA dataset
+  #'                          of the get_bucket_dist function for the full AMELIA dataset.
+  #'   - bucket_size        = Size of income buckets in information about AMELIA full population
+  #'   - max                = maximum income in AMELIA dataset
   #'   - padding            = Potential padding of the relevance, ranges between 0 and 0.5.
   #'                          Determines how much smaller than 0/1 the min/max values are. 
   #'   - damp               = Specifies the density that should be used for dampening the relevance function. 
@@ -184,30 +189,48 @@ compute_income_diff <- function(sample_income, amelia_income_dist, bucket_size,
   #'     - scaled_difference_table = Difference table with scaled values 
   #'     - relevance_table         = Difference in table form with dampening to counteract group size
   #'     - scaled_relevance_table  = Relevance table with scaled values
+  #'     - relevance_matrix_ubl    = Relevance in correct form for functions in UBL package
   #'     - relevance_threshold     = Suggested threshold values for relevance function. Contains mean and
   #'                                 median of the scaled relevance function as a list.
   
-  sample_income_dist <- get_bucket_dist(sample_income, bucket_size = bucket_size)
+  min <- 0
+  max <- max_val + bucket_size
+  
+  buckets <- seq(min, max, bucket_size)
+  
+  sample_income_dist <- get_bucket_dist(sample_income, buckets = buckets)
   
   if (tolower(damp) == "pop") dampening <- 1 - amelia_income_dist$dist_prop
   else if (tolower(damp) == "sample") dampening <- 1 - sample_income_dist$dist_prop
   else{
-    print("Input for 'damp' must be either 'sample' or 'pop'. Defaulting to 'pop'.")
+    message("Input for 'damp' must be either 'sample' or 'pop'. Defaulting to 'pop'.")
     dampening <- 1 - amelia_income_dist$dist_prop
   }
   
   diff <- (amelia_income_dist$dist_prop - sample_income_dist$dist_prop)
+  diff_numeric <- as.numeric(diff)
   relevance <- diff * dampening
   
   # Apply rescaling as UBL relevance function ~ [0,1] and convert back to table
   min <- 0 + padding
   max <- 1 - padding
   
-  diff_scaled <- as.table(rescale(as.numeric(diff), to = c(min, max)))
-  relevance_scaled <- as.table(rescale(as.numeric(diff), to = c(min, max)))
+  diff_scaled <- as.table(rescale(diff_numeric, to = c(min, max)))
+  relevance_scaled <- as.table(rescale(diff_numeric, to = c(min, max)))
   
   names(diff_scaled) <- names(sample_income_dist$dist_prop)
   names(relevance_scaled) <- names(sample_income_dist$dist_prop)
+  
+  # Create matrix for UBL functions 
+  relevance_deriv_per_bucket <- rep(0, times = length(buckets)-1)
+  
+  relevance_matrix_ubl <- matrix(
+    cbind(
+      buckets[1:length(buckets)-1] + 1/2 * bucket_size, # Mean of each bucket as sampling point 
+      as.numeric(relevance_scaled), 
+      relevance_deriv_per_bucket), ncol = 3)
+  
+  
   
   # Suggested Threshold for relevance function: Median of relevance function
   threshold_mean <- mean(as.numeric(relevance_scaled))
@@ -220,6 +243,7 @@ compute_income_diff <- function(sample_income, amelia_income_dist, bucket_size,
     "scaled_difference_table" = diff_scaled,
     "relevance_table" = relevance, 
     "scaled_relevance_table" = relevance_scaled,
+    "relevance_matrix_ubl" = relevance_matrix_ubl,
     "relevance_threshold" = list("mean" = threshold_mean,
                                  "median" = threshold_med))
   
