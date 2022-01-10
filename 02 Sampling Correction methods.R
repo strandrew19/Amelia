4be95f8c422766d9d16004e5fb2900e70be3b21e
David Schischke <hi@davidschischke.com>
2022-01-10 22:48:33 +0100

    Implemented resampling methods myself and added resampled data to the existing sampling data

--- 02 Sampling Correction methods.R
+++ 02 Sampling Correction methods.R
@@ -12,9 +12,8 @@ source("functions/sampling_correction.R")
 
 ##### Get reference values (i.e. 'true' population values) ##### 
 
-
 # Set bucket size for comparison between values 
-BUCKET_SIZE <- 20000
+BUCKET_SIZE <- 5000
 formals(compute_income_diff)$bucket_size <- BUCKET_SIZE # Set as global preset
 
 # Get maximum income 
@@ -24,8 +23,8 @@ formals(compute_income_diff)$max_val <- MAX
 #### Computing true buckets #####
 AMELIA_BUCKET_INCOME <- get_bucket_dist(amelia_full_income, AMELIA = T, max_val = MAX, bucket_size = BUCKET_SIZE)
 formals(compute_income_diff)$amelia_income_dist <- AMELIA_BUCKET_INCOME # Set as global preset
-formals(resample)$amelia_buckets <- AMELIA_BUCKET_INCOME
-
+formals(importance_sample)$amelia_buckets <- AMELIA_BUCKET_INCOME
+formals(synth_sample)$amelia_buckets <- AMELIA_BUCKET_INCOME
 
 #### Simulation #### 
 
@@ -39,17 +38,26 @@ for (file in FILENAMES){
   current_sample <- readRDS(sprintf("data/samples/%s", file))
   for (method in SAMPLING_METHODS){
     print(sprintf("### %s ####", method))
-    
+
     resample_data <- current_sample[[method]]$Base
-    
+    resample_data$Sex <- as.factor(resample_data$Sex)
+
+    # Base error
     income_difference <- compute_income_diff(resample_data$Person_Income)
-    
     print(sprintf("Base Sample difference: %.6f", income_difference$squared_deviation))
     
-    current_sample[[method]]$importance_sampling <- resample(income_difference, data = resample_data, print_cases = F)
-    
+    # Importance Sampling
+    current_sample[[method]]$importance_sampling <- importance_sample(income_difference, data = resample_data, print_cases = F)
     imp_samp_error <- compute_income_diff(current_sample[[method]]$importance_sampling$Person_Income, return_full = F)
+    print(sprintf("Importance sampling difference: %.6f", imp_samp_error))
     
-    print(sprintf("Importance sampling difference: %.8f", imp_samp_error))
+    # Synthetic oversampling
+    current_sample[[method]]$synthetic_sampling <- synth_sample(income_difference, data = resample_data, k = 3, print_cases = F)
+    synth_samp_error <- compute_income_diff(current_sample[[method]]$synthetic_sampling$Person_Income, return_full = F)
+    print(sprintf("Synthetic sampling difference: %.6f", imp_samp_error))
+    
+    # Save
+    filename <- sprintf("data/samples/%s", file)
+    saveRDS(current_sample, filename)
   }
 }
--- functions/sampling_correction.R
+++ functions/sampling_correction.R
@@ -1,13 +1,9 @@
-folder <- dirname(rstudioapi::getSourceEditorContext()$path)
-
-
-
 library(dplyr)
+library(FNN)
 
 ##### Importance Sampling ######
 
-resample <- function(sample_diff, amelia_buckets, data, smoteR = F, print_cases = T){
-  
+importance_sample <- function(sample_diff, amelia_buckets, data, print_cases = T){
   #' Function to perform under- and oversampling per income buckets. 
   #' Since the density for lower income buckets is much higher than for higher income buckets, we
   #' need to put more emphasis on smaller differences in higher income buckets. For this, we 
@@ -16,18 +12,17 @@ resample <- function(sample_diff, amelia_buckets, data, smoteR = F, print_cases 
   #' 
   #' INPUT: 
   #'   - sample_diff    = List object from compute_income_diff
-  #'   - data           = Sample data to modify
   #'   - amelia_buckets = Density of each income bucket in full AMELIA dataset, used for scaling
   #'                      the density per bucket 
-  #'   - smoteR         = Whether or not kNN should be used to generate synthetic data points
-  #'   - print_cases    = Whether function should print replacements per bucket
+  #'   - data           = Sample data to modify
+  #'   - print_cases    = Whether function should print # of replacements per bucket
   #' 
   #' OUTPUT
   #'   - resample_data = data with added/removed data points
   
-  buckets <- sample_diff$buckets                      
+  buckets <- sample_diff$buckets
   proportions <- as.numeric(sample_diff$difference_table_prop)
-  relevance <- as.numeric(sample_diff$relevance_table)
+
   
   #' Copy the data frame to append / subtract the additional samples
   #' without modifying the original data frame. I.e. create a copy, not just
@@ -37,7 +32,8 @@ resample <- function(sample_diff, amelia_buckets, data, smoteR = F, print_cases 
   # Scaling
   scale_factor <- 1/amelia_buckets$dist_prop
   proportions <- proportions * scale_factor
-
+  
+  
   #' buckets begins with 0, i.e. use ith element as lower and i+1th element as upper border. 
   for (i in 1:length(proportions)){
     # Select data in relevant income bucket
@@ -45,6 +41,11 @@ resample <- function(sample_diff, amelia_buckets, data, smoteR = F, print_cases 
   
     # Select amount of necessary over/undersampling based on scaled proportional difference
     sample_proportion <- proportions[i]
+    
+    #' If there is zero people in a bucket, the scaling returns NaN. In these cases, simply correct
+    #' the sampling proportion to 0.
+    if(is.nan(sample_proportion)) sample_proportion <- 0
+    
     
     # Sample additional data    
     add_data <- slice_sample(sample_base, prop = abs(sample_proportion), replace = F)
@@ -63,23 +64,87 @@ resample <- function(sample_diff, amelia_buckets, data, smoteR = F, print_cases 
   return(resample_data) 
 }
 
-# resample_data <- ex_sample$`Stratified Cluster`$Base
-# sample_difference <- compute_income_diff(ex_sample$`Stratified Cluster`$Base$Person_Income, plot = T, plot_relevance = T)
-# 
-# test <- resample(sample_difference, AMELIA_BUCKET_INCOME, data = resample_data)
-# irrel <- compute_income_diff(test$Person_Income, plot = T, plot_relevance = T)
+##### Synthetic Oversampling #####
 
-##### SmoteR ##### 
-
-#' GIVEN
-#'   - Table of data points
-#'   - Proportion of new cases to generate
-#'    
-
-# GenSynthCases
-knn_data <- resample_data %>% filter(Person_Income > 20000 & Person_Income <= 40000)
-
-
-
-
-
+synth_sample <- function(sample_diff, amelia_buckets, k = 4, data, print_cases = T){
+  #' Synthetically generate new data points using the kNN function from the FNN package. 
+  #' Applies the same scaling of relevance to higher values, but does not generate any new 
+  #' data points if there are less data points in the given cluster than new ones would 
+  #' be required. 
+  #' 
+  #' INPUT: 
+  #'   - sample_diff    = List object from compute_income_diff
+  #'   - data           = Sample data to modify
+  #'   - amelia_buckets = Density of each income bucket in full AMELIA dataset, used for scaling
+  #'                      the density per bucket 
+  #'   - smoteR         = Whether or not kNN should be used to generate synthetic data points
+  #'   - print_cases    = Whether function should print # of replacements per bucket
+  #' 
+  #' OUTPUT
+  #'   - resample_data = data with added/removed data points
+  
+  
+  buckets <- sample_diff$buckets                      
+  proportions <- as.numeric(sample_diff$difference_table_prop)
+  
+  
+  # Scaling
+  scale_factor <- 1/amelia_buckets$dist_prop
+  proportions <- proportions * scale_factor
+  
+  #' If there is zero people in a bucket, the scaling returns NaN. In these cases, simply correct
+  #' the sampling proportion to 0.
+  for (i in 1:length(proportions)){
+    if(is.na(proportions[i]) | is.nan(proportions[i])) proportions[i] <- 0
+  }
+  
+  # Since we do kNN, we can only add cases, i.e. shift s.t. lowest value == 0
+  no_of_cases <- proportions * sample_diff$dist_val
+  min_no_of_cases <- min(no_of_cases)
+  no_of_cases <- round(no_of_cases + ifelse(min_no_of_cases < 0, abs(min_no_of_cases), min_no_of_cases))
+  
+  synthetic_cases <- data.frame()
+  for (i in 1:length(proportions)){
+    if(no_of_cases[i] != 0 & no_of_cases[i] < sample_diff$dist_val[i]){
+      # Get all cases in current income bucket
+      knn_base <- data %>% 
+        filter(Person_Income >= buckets[i] & Person_Income < buckets[i+1]) %>% 
+        # Convert to numeric for kNN implementation to work. 
+        mutate_all(function(x) as.numeric(x)) # Potential problem: Do we need Sex as dummy-coding?
+      
+      #' Do random split into to datasets. One contains as many cases as we need to generate new 
+      #' data points. Use these data points as basis, find k-1 nearest neighbours in other cases.
+      new_cases_basis <- knn_base %>% sample_n(no_of_cases[i], replace = F)
+      other_cases <- knn_base %>% filter(!Personal_ID %in% new_cases_basis$Personal_ID)
+      
+      # Get NNs, account for small sample sizes when choosing k
+      nn <- ifelse(k < nrow(other_cases), k, nrow(other_cases))
+      nn_data <- get.knnx(data = other_cases, query = new_cases_basis, k = nn)
+      
+      current_synthethic_cases <- data.frame()
+      for (j in 1:nrow(new_cases_basis)){
+        # Get current case from new_cases basis and it's k nearest neighbours based on their index
+        current_cases <- rbind(new_cases_basis[j,], other_cases[nn_data$nn.index[j,],])
+        
+        # Compute new value as the mean of the k nearest neighbours
+        new_case <- current_cases %>% colMeans() %>% as.data.frame.list()
+        
+        # Append to dataframe
+        current_synthethic_cases <- rbind(current_synthethic_cases, new_case)
+      }
+      
+      if(print_cases) print(sprintf("Bucket %s to %s: Generating %d new cases", buckets[i], buckets[i+1], nrow(current_synthethic_cases)))
+      synthetic_cases <- rbind(synthetic_cases, current_synthethic_cases)
+    }
+  }
+  
+  # Convert integer values back to integer
+  synthetic_cases[sapply(data, class) == "integer",] <- sapply(synthetic_cases[sapply(data, class) == "integer",], as.integer)
+  
+  # Convert sex back to factor by rounding
+  synthetic_cases$Sex <- factor(round(synthetic_cases$Sex), levels = c(1,2), labels = c("Female", "Male"))
+  
+  resample_data <- rbind(data, synthetic_cases)
+  
+  return(resample_data)
+}
