e4ff0246a3642e615314fcc151427f5e0cb6091a
Andrew Stranberg <62930497+strandrew19@users.noreply.github.com>
2022-01-21 17:40:34 +0100

    Update MLPRegressor.py

--- MLPRegressor.py
+++ MLPRegressor.py
@@ -26,15 +26,25 @@ x_test = pd.DataFrame(x_test, columns=x_var.columns)
 
 #Run Multi-layer Perceptron regressor
 regr = MLPRegressor(random_state=420, max_iter=500, activation = "relu", solver='lbfgs').fit(x_train, y_train)
-predictions = regr.predict(x_train)
+predictions = regr.predict(x_test)
 
 #Replace negative outputs with 0
 predictions = np.where(predictions < 0, 0, predictions)
 
 #Generate Score Value
-print(regr.score(x_train, y_train))
+print(regr.score(x_test, y_test))
 
-import seaborn as sns
-import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
-sns.kdeplot(data=predictions, bw_adjust=.2)
+#display distribution in kernel density plot
+#sns.kdeplot(data=predictions, bw_adjust=.2)
+
+#display first 30 predictions vs acutual values
+# df_temp = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
+# df_temp = df_temp.head(30)
+
+# df_temp.plot(kind='bar',figsize=(10,6))
+# plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+# plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+# plt.show()
