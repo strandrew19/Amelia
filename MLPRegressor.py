fcf359a01387484821d3968d9892f2f35922b389
Andrew Stranberg <62930497+strandrew19@users.noreply.github.com>
2022-01-22 15:22:37 +0100

    Update MLPRegressor.py
    Updating for backup

--- MLPRegressor.py
+++ MLPRegressor.py
@@ -6,45 +6,87 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 
-#Read in Sample
-AMELIA = feather.read_dataframe("01_sample.feather")
+#Set top level directory, path for reading feather and output file for the results
+top_wd = os.getcwd()
+feather_dir = top_wd + "\\Feather"
+os.makedirs(top_wd + "\\Results", exist_ok=True)
 
-#Split x and y vars (also drop personal ID identifier)
-x_var = AMELIA.iloc[:,2:].drop(columns = "Personal_ID")
-y_var = AMELIA.iloc[:,0]
+#Create a dictionary for accesing all dataframes
+df_dict = {}
+for i in range(10):
+    os.chdir(feather_dir)
+    SRS = feather.read_dataframe(f"0{i+1}_srs_sample.feather") 
+    Importance = feather.read_dataframe(f"0{i+1}_importance_sample.feather") 
+    Synthetic = feather.read_dataframe(f"0{i+1}_synthetic_sample.feather")
 
-#Creat Train/Test split for x and y
-x_train, x_test, y_train, y_test = train_test_split(x_var, y_var, test_size = 0.25, random_state = 420)
+    #Apply numeric conversion (Male:1, Female:2)
+    SRS = to_numeric(SRS)
+    Importance = to_numeric(Importance)
+    Synthetic = to_numeric(Synthetic)
 
-#Scale x var for train and test
-sc = StandardScaler()
+    df_dict[f"SRS_{i+1}"] = SRS
+    df_dict[f"Importance_{i+1}"] = Importance
+    df_dict[f"Synthetic_{i+1}"] = Synthetic
+   
+#Create a function for handeling train test split
+def t_t_split(dataframe):
+    #Split x and y vars (also drop personal ID identifier)
+    x_var = dataframe.iloc[:,2:].drop(columns = "Personal_ID")
+    y_var = dataframe.iloc[:,0]
 
-x_train = sc.fit_transform(x_train)
-x_test = sc.fit_transform(x_test)
-x_train = pd.DataFrame(x_train, columns=x_var.columns)
-x_test = pd.DataFrame(x_test, columns=x_var.columns)
+    #Creat Train/Test split for x and y
+    x_train, x_test, y_train, y_test = train_test_split(x_var, y_var, test_size = 0.25, random_state = 420)
 
-#Run Multi-layer Perceptron regressor
-regr = MLPRegressor(random_state=420, max_iter=500, activation = "relu", solver='lbfgs').fit(x_train, y_train)
-predictions = regr.predict(x_test)
+    #Keep a copy of unscaled x_test for later comparison
+    x_test_unscaled = x_test
 
-#Replace negative outputs with 0
-predictions = np.where(predictions < 0, 0, predictions)
+    #Scale x var for train and test
+    sc = StandardScaler()
+    #Done now for convience regarding column reassignment(rather than above when x_var created)
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.fit_transform(x_test)
+    x_train = pd.DataFrame(x_train, columns=x_var.columns)
+    x_test = pd.DataFrame(x_test, columns=x_var.columns)
 
-#Generate Score Value
-print(regr.score(x_test, y_test))
+    return x_train, x_test, y_train, y_test, x_test_unscaled
+  
+  #Loop through Sampling methods and run our NN on each one, storing the score and results
+def results(df_dict, Method = "SRS"):
 
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+   s_names = ["SRS", "Importance", "Synthetic"]
 
-#display distribution in kernel density plot
-#sns.kdeplot(data=predictions, bw_adjust=.2)
+   scores = pd.DataFrame(columns = s_names)
+   predicts_dict = {}
 
-#display first 30 predictions vs acutual values
-# df_temp = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
-# df_temp = df_temp.head(30)
+   """For Method pass: SRS, Importance or Synthetic (SRS Default)"""
+   for i in range(10):   
+      x_train, x_test, y_train, y_test, x_test_unscaled = t_t_split(df_dict[f"{Method}_{i+1}"])
 
-# df_temp.plot(kind='bar',figsize=(10,6))
-# plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
-# plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
-# plt.show()
+      #Run Multi-layer Perceptron regressor
+      regr = MLPRegressor(random_state=420, max_iter=500, activation = "relu", solver='lbfgs').fit(x_train, y_train)
+      
+      #Generate Score Value
+      s_v = regr.score(x_test, y_test)
+
+      #Write If function ( if SRS: if != SRS:)
+
+      #Add score to scores DataFrame
+      scores = scores.append({Method:s_v}, ignore_index=True)
+
+      #Once SRS is done 
+      # scores.loc[i,f"{Method}"] = scores
+
+      #Generate Predictions
+      predictions = regr.predict(x_test)
+      
+      #Replace negative outputs with 0
+      predictions = np.where(predictions < 0, 0, predictions)
+
+      #Convert Predicitons from Ndarray to Dataframe for Concat
+      predictions = pd.DataFrame(data=predictions, columns=["Predictions"])
+      
+      #Store Y_test, Predictions for X_test and data for X_test (For Comparision)
+      fused_df = pd.concat([y_test.reset_index(drop=True), predictions, x_test_unscaled.reset_index(drop=True)], axis = 1)
+      predicts_dict[f"{Method}_{i+1}"] = fused_df
+   
+   return scores, predicts_dict
