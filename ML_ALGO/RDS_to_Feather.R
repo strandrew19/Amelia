7b2a1ac57493fb35cd67f3af532d3855eb5286d2
Andrew Stranberg <62930497+strandrew19@users.noreply.github.com>
2022-01-26 10:38:54 +0100

    Update RDS_to_Feather.R
    Updated to Include Stratified and Cluster Samples

--- ML_ALGO/RDS_to_Feather.R
+++ ML_ALGO/RDS_to_Feather.R
@@ -5,42 +5,46 @@ library(feather)
 #Save root working directory
 root_dir <- getwd()
 
+S_Method <- c("SRS", "Stratified", "Cluster")
+
 #Code for unpacking sampled RDS files and writing them as feather
-for (i in c(1:10)){
-  if (i < 10){
-  #RDS data samples should be stored in a folder "RDS"!!
-  setwd(sprintf(root_dir, "/ML Algo/RDS"))
-  srs <- readRDS(paste0("0", i, "_sample.rds"))$SRS$Base
-  importance <- readRDS(paste0("0", i, "_sample.rds"))$SRS$importance_sampling
-  synthetic <- readRDS(paste0("0", i, "_sample.rds"))$SRS$synthetic_sampling
- 
-  #Scale data
-  srs = srs %>% relocate(Person_Income) # move Y column to first position
-  importance = importance %>% relocate(Person_Income)
-  synthetic = synthetic %>% relocate(Person_Income)
-  
-  #Create Feather Directory under ML Algo!!
-  setwd(sprintf(root_dir, "/ML Algo/Feather"))
-  write_feather(srs, paste0("0", i, "_srs_sample.feather"))
-  write_feather(importance, paste0("0", i, "_importance_sample.feather"))
-  write_feather(synthetic, paste0("0", i, "_synthetic_sample.feather"))
-  }
-  
-  #due to indexing, if statement is required (instead of 010 we have 10 in this loop)
-  if (i >= 10){
+for (j in c(1:3)){
+  for (i in c(1:10)){
+    if (i < 10){
+    #RDS data samples should be stored in a folder "RDS"!!
     setwd(sprintf(root_dir, "/ML Algo/RDS"))
-    srs <- readRDS(paste0(i, "_sample.rds"))$SRS$Base
-    importance <- readRDS(paste0(i, "_sample.rds"))$SRS$importance_sampling
-    synthetic <- readRDS(paste0(i, "_sample.rds"))$SRS$synthetic_sampling
-       
-    srs = srs %>% relocate(Person_Income) 
+    base <- readRDS(paste0("0", i, "_sample.rds"))[[j]]$Base
+    importance <- readRDS(paste0("0", i, "_sample.rds"))[[j]]$importance_sampling
+    synthetic <- readRDS(paste0("0", i, "_sample.rds"))[[j]]$synthetic_sampling
+
+    # 2. Scale data
+    base = base %>% relocate(Person_Income) # move Y column to first position
     importance = importance %>% relocate(Person_Income)
     synthetic = synthetic %>% relocate(Person_Income)
-    
+
+    #Create Feather Directory under ML Algo!!
     setwd(sprintf(root_dir, "/ML Algo/Feather"))
-    write_feather(srs, paste0(i, "_srs_sample.feather"))
-    write_feather(importance, paste0(i, "_importance_sample.feather"))
-    write_feather(synthetic, paste0(i, "_synthetic_sample.feather"))  
+    write_feather(base, paste0("0", i, "_", s_Method[j],"_base_sample.feather"))
+    write_feather(importance, paste0("0", i, "_", S_Method[j], "_importance_sample.feather"))
+    write_feather(synthetic, paste0("0", i, "_", S_Method[j], "_synthetic_sample.feather"))
+    }
+  
+    #due to indexing, if statement is required (instead of 010 we have 10 in this loop)
+    if (i >= 10){
+      setwd(sprintf(root_dir, "/ML Algo/RDS"))
+      base <- readRDS(paste0(i, "_sample.rds"))[[j]]$Base
+      importance <- readRDS(paste0(i, "_sample.rds"))[[j]]$importance_sampling
+      synthetic <- readRDS(paste0(i, "_sample.rds"))[[j]]$synthetic_sampling
+
+      base = base %>% relocate(Person_Income) 
+      importance = importance %>% relocate(Person_Income)
+      synthetic = synthetic %>% relocate(Person_Income)
+
+      setwd(sprintf(root_dir, "/ML Algo/Feather"))
+      write_feather(base, paste0("0", i, "_", s_Method[j],"_base_sample.feather"))
+      write_feather(importance, paste0("0", i, "_", S_Method[j], "_importance_sample.feather"))
+      write_feather(synthetic, paste0("0", i, "_", S_Method[j], "_synthetic_sample.feather"))
+    }
   }
-}
+ }
 
