91fc3ee60e669c784804adf920f23e9ffa6f5ee8
David Schischke <64317824+daaawit@users.noreply.github.com>
2022-02-04 08:39:16 +0100

    Refactorization + Saving to Feather
    Changed file output from RDS to feather for usability with Python ML models. Also changed resampling methods to package instead of separate scripts.

--- 02 Sampling Correction methods.R
+++ 02 Sampling Correction methods.R
@@ -1,14 +1,13 @@
-# Read data first as RStudio gets stuck on read_feather command otherwise
 wd <- dirname(rstudioapi::getSourceEditorContext()$path)
-amelia_full_income <- arrow::read_feather(sprintf("%s/data/AMELIA.feather", wd))$Person_Income
-
 setwd(wd)
+
+library(arrow)
 library(dplyr)
-library(UBL)
 library(stringr)
-source("functions/relevance_function.R")
-source("functions/relevance_function_plot.R")
-source("functions/sampling_correction.R")
+devtools::load_all("functions/SamplingCorrection")
+
+amelia_full_income <- read_feather(sprintf("%s/data/AMELIA.feather", wd))$Person_Income
+
 
 ##### Get reference values (i.e. 'true' population values) ##### 
 
@@ -16,16 +15,9 @@ source("functions/sampling_correction.R")
 
 # Source: https://unstats.un.org/unsd/demographic/sources/census/quest/NZL2013enIn.pdf
 BUCKETS <- c(seq(0, 40000, by = 5000), seq(50000, 70000, by = 10000), 100000, 150000, max(amelia_full_income)+1)
-formals(compute_income_diff)$buckets <- BUCKETS # Set as global preset
-formals(importance_sample)$buckets <- BUCKETS
-formals(synth_sample)$buckets <- BUCKETS
 
 #### Computing true buckets #####
 AMELIA_BUCKET_INCOME <- get_bucket_dist(amelia_full_income, buckets = BUCKETS)
-
-formals(compute_income_diff)$amelia_income_dist <- AMELIA_BUCKET_INCOME # Set as global preset
-formals(importance_sample)$amelia_buckets <- AMELIA_BUCKET_INCOME
-formals(synth_sample)$amelia_buckets <- AMELIA_BUCKET_INCOME
 
 #### Simulation #### 
 
@@ -39,30 +31,59 @@ for (file in FILENAMES){
   print(sprintf("---------- %s ----------", file))
   set.seed(seed)
   current_sample <- readRDS(sprintf("data/samples/%s", file))
+  
+  current_iter <- str_pad(seed, width = 2, pad = "0")
+  
   for (method in SAMPLING_METHODS){
     print(sprintf("### %s ####", method))
 
-    resample_data <- current_sample[[method]]$Base
+    resample_data <- current_sample[[method]]
     resample_data$Sex <- as.factor(resample_data$Sex)
+    resample_data <- relocate(resample_data, Person_Income) # Put DV as first column for ML
 
     # Base error
-    income_difference <- compute_income_diff(resample_data$Person_Income, plot = T, plot_relevance = T)
+    income_difference <- compute_income_diff(resample_data$Person_Income,
+                                             amelia_income_dist = AMELIA_BUCKET_INCOME,
+                                             buckets = BUCKETS,
+                                             plot = T, 
+                                             plot_relevance = T)
     print(sprintf("Base Sample difference: %.6f", income_difference$squared_deviation))
     
+    ### Saving to feather ####
+    filename <- sprintf("%s/data/Feather Samples/%s_%s_base_sample.feather", wd, current_iter, method)
+    write_feather(resample_data, filename)
+    
     # Importance Sampling
-    current_sample[[method]]$importance_sampling <- importance_sample(income_difference, data = resample_data, print_cases = F)
-    imp_samp_error <- compute_income_diff(current_sample[[method]]$importance_sampling$Person_Income, return_full = F)
+    importance_sampling <- importance_sample(income_difference, 
+                                             data = resample_data,
+                                             amelia_buckets = AMELIA_BUCKET_INCOME,
+                                             buckets = BUCKETS,
+                                             print_cases = F)
+    imp_samp_error <- compute_income_diff(importance_sampling$Person_Income, 
+                                          amelia_income_dist = AMELIA_BUCKET_INCOME,
+                                          buckets = BUCKETS,
+                                          return_full = F)
     print(sprintf("Importance sampling difference: %.6f", imp_samp_error))
     
+    ### Saving to feather ####
+    filename <- sprintf("%s/data/Feather Samples/%s_%s_importance_sample.feather", wd, current_iter, method) 
+    write_feather(importance_sampling, filename)
+    
     # Synthetic oversampling
-    current_sample[[method]]$synthetic_sampling <- synth_sample(income_difference, data = resample_data, k = 3, print_cases = F)
-    synth_samp_error <- compute_income_diff(current_sample[[method]]$synthetic_sampling$Person_Income, return_full = F)
+    synthetic_sampling <- synth_sample(income_difference, 
+                                       data = resample_data,
+                                       amelia_buckets = AMELIA_BUCKET_INCOME,
+                                       buckets = BUCKETS,
+                                       print_cases = F)
+    synth_samp_error <- compute_income_diff(synthetic_sampling$Person_Income, 
+                                            amelia_income_dist = AMELIA_BUCKET_INCOME,
+                                            buckets = BUCKETS,
+                                            return_full = F) 
     print(sprintf("Synthetic sampling difference: %.6f", imp_samp_error))
+    
+    
+    ### Saving to feather ####
+    filename <- sprintf("%s/data/Feather Samples/%s_%s_synthetic_sample.feather", wd, current_iter, method) 
+    write_feather(synthetic_sampling, filename)
   }
-  
-  # Save
-  filename <- sprintf("data/samples/%s", file)
-  saveRDS(current_sample, filename)
-  
-  seed <- seed + 1
 }
