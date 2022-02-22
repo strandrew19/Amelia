67f9400fb17a5c8b4ce0b77d3c23e50453193b3e
David Schischke <s1ddschi@uni-trier.de>
2022-02-22 13:16:58 +0100

    Fixed evaluation plot

--- 04 Analysis.R
+++ 04 Analysis.R
@@ -1,5 +1,9 @@
 library(tidyverse)
-library()
+
+wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+scores <- read.csv(sprintf("%s/results/scores.csv", wd))
+scores_holdout <- read.csv(sprintf("%s/results/scores_holdout.csv", wd))
 
 scores_long <- pivot_longer(scores, cols = -X, names_to = c("Regression", "Sample", "Correction"), names_sep = "_")
 holdout_long <- pivot_longer(scores_holdout, cols = -X, names_to = c("Regression", "Sample", "Correction"), names_sep = "_")
@@ -8,8 +12,11 @@ scores_long$sample <- "test"
 holdout_long$sample <- "holdout"
 
 long <- rbind(scores_long, holdout_long)
+long$Regression <- tolower(long$Regression)
+long$Sample <- tolower(long$Sample)
+long$Correction <- tolower(long$Correction)
 
 ggplot(long, aes(x = Sample, y = value, fill = Correction))+
   geom_boxplot() +
-  facet_wrap(~Regression + sample) +
+  facet_wrap(~ sample + Regression) +
   jtools::theme_apa()
