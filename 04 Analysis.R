0a94a52b7717094c3d990a26f45ea3df367b9352
David Schischke <hi@davidschischke.com>
2022-02-23 09:49:02 +0100

    Updated results

--- 04 Analysis.R
+++ 04 Analysis.R
@@ -1,5 +1,9 @@
 library(tidyverse)
-library()
+wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+scores <- read.csv(sprintf("%s/results/scores.csv", wd), sep = ";")
+scores_holdout <- read.csv(sprintf("%s/results/scores_holdout.csv", wd), sep = ";")
+
 
 scores_long <- pivot_longer(scores, cols = -X, names_to = c("Regression", "Sample", "Correction"), names_sep = "_")
 holdout_long <- pivot_longer(scores_holdout, cols = -X, names_to = c("Regression", "Sample", "Correction"), names_sep = "_")
@@ -11,5 +15,12 @@ long <- rbind(scores_long, holdout_long)
 
 ggplot(long, aes(x = Sample, y = value, fill = Correction))+
   geom_boxplot() +
-  facet_wrap(~Regression + sample) +
+  facet_wrap(~sample + Regression) +
+  jtools::theme_apa()
+
+# Quality of base prediction
+
+ggplot(filter(long, Correction %in% c("Base", "base")), aes(x = Sample, y = value, fill = sample))+
+  geom_boxplot() +
+  facet_wrap(~Regression) +
   jtools::theme_apa()
